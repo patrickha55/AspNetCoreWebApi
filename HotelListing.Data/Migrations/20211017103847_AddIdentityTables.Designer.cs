9cbbc362b38aa414b731cd02ed1e14d8010b4b1f
Phat <pattrickha@gmail.com>
2021-10-17 17:39:39 +0700

    Added Identity Migration

--- HotelListing.Data/Migrations/20211017103847_AddIdentityTables.Designer.cs
+++ HotelListing.Data/Migrations/20211017103847_AddIdentityTables.Designer.cs
@@ -4,14 +4,16 @@ using HotelListing.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelListing.Data.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20211017103847_AddIdentityTables")]
+    partial class AddIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
