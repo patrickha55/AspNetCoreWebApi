796daedce8dedc52837c0a49ff4b2b139fae10c7
patrickha55 <pattrickha@gmail.com>
2021-10-17 12:14:47 +0700

    Added GetCountry and HotelsController

--- HotelListing.Webapi/Controllers/CountriesController.cs
+++ HotelListing.Webapi/Controllers/CountriesController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -27,7 +28,10 @@ namespace HotelListing.Webapi.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetCountries()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<CountryDTO>>> GetCountries()
         {
             try
             {
@@ -42,6 +46,29 @@ namespace HotelListing.Webapi.Controllers
             catch (Exception e)
             {
                 _logger.LogError(exception: e,$"Something went wrong in the {nameof(GetCountries)} method in the CountriesController.");
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+        
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<CountryDTO>> GetCountry(int id)
+        {
+            try
+            {
+                var country = await _unitOfWork.Countries.Get(c => c.Id == id, new List<string> {"Hotels"});
+
+                if (country is null) return NotFound();
+                
+                var result = _mapper.Map<CountryDTO>(country);
+                
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(exception: e,$"Something went wrong in the {nameof(GetCountry)} method in the CountriesController.");
                 return StatusCode(500, "Internal Server Error. Please try again later.");
             }
         }
